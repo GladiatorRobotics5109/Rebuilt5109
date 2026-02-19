53a6eb6207ea381ac9f724d7e8639d3ea0271a27
Nathan Q <Natethegreat08q@gmail.com>
2026-02-19 12:28:19 -0500

    pop stash before merge of "VisionTesting" and "origin/VisionTesting"

--- src/main/java/frc/robot/commands/AutoBuilder.java
+++ src/main/java/frc/robot/commands/AutoBuilder.java
@@ -1,4 +1,9 @@
 package frc.robot.commands;
 
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.wpilibj2.command.Command;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class AutoBuilder {
 }
