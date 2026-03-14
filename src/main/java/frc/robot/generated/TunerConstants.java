16d04eb89c1357b5642739c43b2c59133fc85a78
Gladiator Robotics <jchsrobotics@gmail.com>
2026-03-14 08:22:51 -0400

    Update at 'Sat Mar 14 08:22:51 EDT 2026'

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -32,8 +32,8 @@ public class TunerConstants {
     //     .withKS(0).withKV(0.124);
 
     private static final Slot0Configs driveGains = new Slot0Configs()
-       .withKP(0.1).withKI(0).withKD(0)
-       .withKS(0).withKV(1.25); // 1.3846480048994894211892887749227
+        .withKP(0.1).withKI(0).withKD(0)
+        .withKS(0).withKV(1.0); // 1.3846480048994894211892887749227
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
