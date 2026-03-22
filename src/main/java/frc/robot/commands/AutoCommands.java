63c3dbb0c891406d1a5c7a1e9a4c6c6b8df3d46e
Gladiator Robotics <jchsrobotics@gmail.com>
2026-03-22 15:09:08 -0400

    Update at 'Sun Mar 22 15:09:08 EDT 2026'

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -188,11 +188,10 @@ public class AutoCommands {
             IndexerCommands.stop(indexer),
             Commands.parallel(
                 AutoBuilder.followPath(kPreloadAndCenter),
-                IntakeCommands.deploy(intake).beforeStarting(Commands.waitSeconds(1.65)),
+                Commands.sequence(IntakeCommands.deploy(intake).beforeStarting(Commands.waitSeconds(1.65)), IntakeCommands.stow(intake).beforeStarting(Commands.waitSeconds(11.5))),
                 TurretCommands.autoAim(turret),
                 FlywheelsCommands.autoAim(flywheels),
-                IndexerCommands.index(indexer).beforeStarting(Commands.waitSeconds(12.0)),
-                IntakeCommands.stow(intake).beforeStarting(Commands.waitSeconds(11.5))
+                IndexerCommands.index(indexer).beforeStarting(Commands.waitSeconds(12.0))
             )
         ).withName("AutoCommands::preloadAndCenterLeft");
     }
