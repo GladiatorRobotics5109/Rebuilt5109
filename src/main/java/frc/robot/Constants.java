7a9176a14c6d410543ea9ea6ad91a6475bf00d14
Gladiator Robotics <jchsrobotics@gmail.com>
2026-03-27 17:44:21 -0400

    Update at 'Fri Mar 27 17:44:21 EDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -127,7 +127,7 @@ public final class Constants {
         public static final Rotation2d kMaxPosition = Rotation2d.kCCW_Pi_2;
         public static final Rotation2d kMinPosition = Rotation2d.kCW_Pi_2;
 
-        public static final Rotation2d kRightTrenchPosition = Rotation2d.fromDegrees(-8);
+        public static final Rotation2d kRightTrenchPosition = Rotation2d.fromDegrees(-10);
         public static final Rotation2d kLeftTrenchPosition = kRightTrenchPosition.times(-1);
         public static final Rotation2d kOutpostPosition = Rotation2d.fromDegrees(15);
 
@@ -331,7 +331,7 @@ public final class Constants {
             LinesHorizontal.center / 4 * 3
         );
 
-        public static final double kTrenchFlywheelsVelocityRPM = 3425.0;
+        public static final double kTrenchFlywheelsVelocityRPM = 3410.0;
     }
 
     public static enum Mode {
